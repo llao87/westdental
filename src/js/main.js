b683eca68e9ae6d49745c85ca156919fac20ed6a
Dmitry Kragel <dmitry.kragel@gmail.com>
2022-12-11 02:54:14 +0300

    added html skeleton of section-about

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,9 @@ jQuery(function () {
     const homeSlider = new Swiper(".home-slider .swiper", {
         loop: true,
     });
+
+    const specialistsSlider = new Swiper(".specialists-slider .swiper", {
+        loop: true,
+        slidesPerView: 6,
+    });
 });
